aa675ffdf91c42468258b8bb1a021847ba4ce425
stevenbrown456 <43067271+stevenbrown456@users.noreply.github.com>
2018-09-21 15:19:23 +0100

    Random Critters Spawn

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public List<GameObject> critterPrefabs;
+    public float critterSpwanFrequency = 1.0f;
+    public Score scoreDisplay; 
+
+    private float lastCritterSpawn = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //check if its time to spawn the next critter 
+        float nextSpawnTime = lastCritterSpawn + critterSpwanFrequency;
+        if ( Time.time >= lastCritterSpawn + critterSpwanFrequency)
+
+        {
+            //it is time 
+
+            //choose a random critter to spawn
+            int critterIndex = Random.Range(0, critterPrefabs.Count);
+            GameObject prefabToSpawn = critterPrefabs[critterIndex];
+
+            //spawn the critter
+            GameObject spawnedCritter = Instantiate(prefabToSpawn);
+
+            //get access to our critter script
+
+            Critter critterScript = spawnedCritter.GetComponent<Critter>();
+
+            //tell the critte scrpit what the score object is
+            critterScript.scoreDisplay = scoreDisplay;
+
+            //update the most recent spawn to now
+            lastCritterSpawn = Time.time;
+        
+
+        }
+	}
+}
