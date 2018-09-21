d16991913d1ab937931387ae1e21226c3d45c9ab
stevenbrown456 <43067271+stevenbrown456@users.noreply.github.com>
2018-09-21 13:56:36 +0100

    critter disappears when clicked

---
+++ Assets/Scripts/Critter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Critter : MonoBehaviour {
+
+    public Vector2 lowerRange;
+    public Vector2 upperRange;
+
+
+    // Use this for initialization
+    void Start () {
+        transform.position = new Vector3(Random.Range(lowerRange.x, upperRange.x),
+            Random.Range(lowerRange.y, upperRange.y),
+            0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //unity calls this when gameobject is clicked
+    void OnMouseDown()
+    {
+        Destroy(gameObject);
+
+    }
+
+}
