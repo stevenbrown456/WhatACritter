457c3b06382ffaad49176c5fb951b2063e11d680
stevenbrown456 <43067271+stevenbrown456@users.noreply.github.com>
2018-09-21 16:51:15 +0100

    Added Movement

--- Assets/Scripts/Critter.cs
+++ Assets/Scripts/Critter.cs
@@ -12,15 +12,29 @@ public class Critter : MonoBehaviour {
 
     public int pointValue = 2;
 
+    private Vector2 direction; //direction critter travels
+
     // Use this for initialization
     void Start () {
         transform.position = new Vector3(Random.Range(lowerRange.x, upperRange.x),
             Random.Range(lowerRange.y, upperRange.y),
             0);
-	}
+
+        //Pick a random direction
+        direction.x = Random.Range(-1.0f, 1.0f);
+        direction.y = Random.Range(-1.0f, 1.0f);
+        direction = direction.normalized;
+
+        //rotate our critter in this direction
+        transform.rotation = Quaternion.FromToRotation(Vector3.up, direction);
+    }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    
+{
+        transform.Translate(Vector2.up * Time.deltaTime);
+
         if (timer.IsTimerRunning()== false)
         {
 
